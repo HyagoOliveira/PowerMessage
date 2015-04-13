f52d75499d6e61432f2c722c7d9e9cc9deb03d53
Mikael Lima <mkmikael07@gmail.com>
2015-04-13 17:16:24 -0300

    Alteração nas views e controllers de Pessoa

--- grails-app/controllers/com/acception/powermessage/PessoaController.groovy
+++ grails-app/controllers/com/acception/powermessage/PessoaController.groovy
@@ -7,14 +7,15 @@ import org.springframework.dao.DataIntegrityViolationException
 class PessoaController {
 
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+    def springSecurityService;
 
     def index() {		
         redirect(action: "list", params: params)
     }
 
     def list(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        [pessoaInstanceList: Pessoa.ativos.list(params), pessoaInstanceTotal: Pessoa.ativos.count()]
+        def pessoas = springSecurityService.currentUser.pessoas.findAll { it.ativo == true }
+        [pessoaInstanceList: pessoas.sort { it.nome }, pessoaInstanceTotal: pessoas.size()]
     }
 
     def create() {		
@@ -23,24 +24,21 @@ class PessoaController {
 
     def save() {
         def pessoaInstance = new Pessoa(params)
-		
-		if(params?.ddd.getClass().isArray()){		
+        springSecurityService.currentUser.addToPessoas(pessoaInstance)
+		if (params?.ddd.getClass().isArray()) {
 			for(int i=0; i<params?.ddd.size();i++){
 				pessoaInstance.addToTelefones(new Telefone(ddd:params?.ddd[i], numero: params?.numero[i]));
 			}
-		}
-		else
-			pessoaInstance.addToTelefones(new Telefone(params));	
-		
-		
-		
-		
-        if (!pessoaInstance.save(flush:true)) {
+		} else {
+			pessoaInstance.addToTelefones(new Telefone(params));
+        }
+
+        if (!pessoaInstance.save(flush: true)) {
             render(view: "create", model: [pessoaInstance: pessoaInstance])
             return
         }
 
-        flash.message = message(code: 'default.created.message', args: [message(code: 'pessoa.label', default: 'Pessoa'), pessoaInstance.nome])
+        flash.message = "O contato ${pessoaInstance.nome} foi salvo com sucesso!"
         redirect(action: "show", id: pessoaInstance.id)
     }
 
@@ -102,7 +100,7 @@ class PessoaController {
             return
         }
 
-        flash.message = message(code: 'default.updated.message', args: [message(code: 'pessoa.label', default: 'Pessoa'), pessoaInstance.id])
+        flash.message = "O contato ${pessoaInstance.nome} foi alterado com sucesso!"
         redirect(action: "show", id: pessoaInstance.id)
     }
 
@@ -115,12 +113,10 @@ class PessoaController {
         }
 
         try {
-            //pessoaInstance.delete(flush: true)
 			pessoaInstance.desativar()
             flash.message = message(code: 'default.deleted.message', args: [message(code: 'pessoa.label', default: 'Pessoa'), id])
             redirect(action: "list")
-        }
-        catch (DataIntegrityViolationException e) {
+        } catch (DataIntegrityViolationException e) {
             flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'pessoa.label', default: 'Pessoa'), id])
             redirect(action: "show", id: id)
         }
